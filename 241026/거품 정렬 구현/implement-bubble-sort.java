ad9bb6c74332bd45ff6c02813d53825d440cd652
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2024-10-26 17:26:01 +0900

    Title: 거품 정렬 구현, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241026/거품 정렬 구현/implement-bubble-sort.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean isSorted;
+        do {
+            isSorted = true;
+            for (int i = 0; i < n-1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+        } while (!isSorted);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
