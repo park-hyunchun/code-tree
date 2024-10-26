e7fb935ae1780aeab7fd11ed23cb701b5daa2d79
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2024-10-26 17:42:41 +0900

    Title: 선택 정렬 구현, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241026/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int minIdx;
+        for (int i = 0; i < n-1; i++) {
+            minIdx = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
