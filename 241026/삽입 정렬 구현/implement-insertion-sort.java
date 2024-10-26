1ae6bf315b2a2abae8e89935220bc07ec96f94e5
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2024-10-26 17:56:37 +0900

    Title: 삽입 정렬 구현, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241026/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int key;
+        int j;
+        for (int i = 1; i < n; i++) {
+            j = i-1;
+            key = arr[i];
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
