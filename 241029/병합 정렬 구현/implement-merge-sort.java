aec36ca68becfe54d748bb87816a345d886b6826
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2024-10-29 20:41:10 +0900

    Title: 병합 정렬 구현, Time: 659ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241029/병합 정렬 구현/implement-merge-sort.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mergeSort(arr, 0, n-1);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        
+    }
+
+    private static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        int[] mergedArr = new int[n];
+        int i = low;
+        int j = mid + 1;
+
+        int k = low;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                mergedArr[k++] = arr[i++];
+            } else {
+                mergedArr[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            mergedArr[k++] = arr[i++];
+        }
+
+        while (j <= high) {
+            mergedArr[k++] = arr[j++];
+        }
+
+        for (k = low; k <= high; k++) {
+            arr[k] = mergedArr[k];
+        }
+    }
+}
