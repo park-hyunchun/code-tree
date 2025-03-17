f69d029d8392a7f30a3a305b8e0754b0021c8467
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2025-03-17 23:27:30 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

--- 250317/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
+++ 250317/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOExeption;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,7 +9,7 @@ public class Main {
     static int[][] map;
     static int result;
 
-    public static void main(String[] args) throws IOExeption {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,11 +17,58 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        map = new int[N][M];
+        result = Integer.MIN_VALUE;
+
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        int sum1;
+        int sum2;
+        for (int r1 = 0; r1 < N; r1++) {
+            for (int c1 = 0; c1 < M; c1++) {
+                for (int r2 = r1; r2 < N; r2++) {
+                    for (int c2 = c1; c2 < M; c2++) {
+
+                        sum1 = 0;
+                        for (int i = r1; i <= r2; i++) {
+                            for (int j = c1; j <= c2; j++) {
+                                sum1 += map[i][j];
+                            }
+                        }
+
+                        for (int r3 = 0; r3 < N; r3++) {
+                            for (int c3 = 0; c3 < M; c3++) {
+
+                                if (r3 <= r2 && c3 <= c2) continue;
+
+                                for (int r4 = r3; r4 < N; r4++) {
+                                    for (int c4 = c3; c4 < M; c4++) {
+                                        
+                                        sum2 = 0;
+                                        for (int i = r3; i <= r4; i++) {
+                                            for (int j = c3; j <= c4; j++) {
+                                                sum2 += map[i][j];
+                                            }
+                                        }
+
+                                        result = Math.max(result, sum1 + sum2);
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+
     }
+
 }
