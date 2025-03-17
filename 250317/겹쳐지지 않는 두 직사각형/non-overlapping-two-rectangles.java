f92fec5e5f93c4ae3aec7953d0a55227dbe6c017
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2025-03-17 22:36:05 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250317/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOExeption;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] map;
+    static int result;
+
+    public static void main(String[] args) throws IOExeption {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                
+            }
+        }
+
+    }
+}
