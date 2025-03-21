fd789a7d503e8ddafb1595e5d14f1d539b9007c4
park-hyunchun <90617511+park-hyunchun@users.noreply.github.com>
2025-03-21 22:55:29 +0900

    Title: 1차원 바람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250321/1차원 바람/The-1D-wind-blows.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, Q;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(Sysetem.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                
+            }
+        }
+
+    }
+}
